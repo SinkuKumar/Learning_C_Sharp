f00011d6ea5fcb7ed3fced7d5fa0b38e3d003c82
sinkukumar <sudosinku@gmail.com>
2024-08-29 10:35:31 +0530

    Create text file analyzer

---
+++ BeginnerProjects/TextFileAnalyzer/TextFileAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace BeginnerProjects.TextFileAnalyzer
+{
+    class TextFileAnalyzer
+    {
+        static void Main(string[] args)
+        {
+            int charCount = 0;
+            int wordCount = 0;
+            int sentenceCount = 0;
+            int paragraphCount = 1;
+            Console.WriteLine("Welcome to text file analyzer.\n");
+            Console.Write("Enter the path of the file: ");
+            string filePath = Console.ReadLine();
+            List<string> fileContent = new List<string>(File.ReadAllLines(filePath));
+            sentenceCount = fileContent.Count;
+
+            foreach (string line in fileContent)
+            {
+                charCount += line.Length;
+                if (line.Length == 0)
+                {
+                    wordCount--;
+                    paragraphCount++;
+                }
+                wordCount += line.Split(' ').Length;
+            }
+
+            Console.WriteLine("\nFile analysis:");
+            Console.WriteLine($"Character(s): {charCount}");
+            Console.WriteLine($"Word(s): {wordCount}");
+            Console.WriteLine($"Sentence(s): {sentenceCount}");
+            Console.WriteLine($"Paragraph(s): {paragraphCount}");
+            Console.WriteLine("\nThank you for using file analyzer.");
+        }
+    }
+}
